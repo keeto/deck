6842f4336004d9b43eebedb078c31825e7f37d2e
Mark Obcena <markeeto@gmail.com>
2010-09-03 11:41:43 +0800

    - Removing options from Deck.setup--it's not essential nor was it   pretty.

--- Source/deck.js
+++ Source/deck.js
@@ -37,44 +37,35 @@ var engineer = function(eng){
 
 };
 
-exports.setup = function(global, engine, options){
+exports.setup = function(global, engine){
 
 	if (global == null || engine == null){
 		throw new TypeError('Deck.setup requires 2 arguments, `global` and `engine`.');
 	}
 
-	options = options || {};
-	var path = options.path || '.',
-		loadModules = (options.loadModules != undefined) ? options.loadModules : true,
-		modulePath = options.modulePath || path + '/modules/',
-		enginePath = options.enginePath || path + '/engines/',
-		vendorPath = options.vendorPath || path + '/thirdparty/';
-
-	require(vendorPath + 'mootools').into(global);
+	require('./thirdparty/mootools').into(global);
 
 	var Engine = (typeof engine === 'string') ? engineer(engine) : engine;
 	if (!Engine) throw new TypeError('Deck.setup can\'t import engine adapter "' + engine + '". Check if you\'re importing the correct adapter.');
 	Engine.Base.global = global;
-	Engine.Vars.Deck = {path: path, env: {deck: deck}};
+	Engine.Vars.Deck = {env: {deck: deck}};
 	global.Engine = Engine;
 
-	var Deck = Object.append(require(path + '/lib/base').Base, {
+	var Deck = Object.append(require('./lib/base').Base, {
 		Info: deck,
 		Modules: {}
 	});
 
-	if (loadModules){
-		var module;
-		Deck.Modules = require(modulePath + 'manifest').Modules;
-		for (var name in Deck.Modules){
-			if (name == 'Classes') continue;
-			module = Array.from(Deck.Modules[name]);
-			Deck.Modules[name] = require(modulePath + module[0])[module[1] || name];
-		}
-		for (var klass in Deck.Modules.Classes){
-			module = Array.from(Deck.Modules.Classes[klass]);
-			Deck.Modules[klass + 'Class'] = Deck.Modules.Classes[klass] = require(modulePath + module[0])[module[1] || klass];
-		}
+	var module;
+	Deck.Modules = require('./modules/manifest').Modules;
+	for (var name in Deck.Modules){
+		if (name == 'Classes') continue;
+		module = Array.from(Deck.Modules[name]);
+		Deck.Modules[name] = require('./modules/' + module[0])[module[1] || name];
+	}
+	for (var klass in Deck.Modules.Classes){
+		module = Array.from(Deck.Modules.Classes[klass]);
+		Deck.Modules[klass + 'Class'] = Deck.Modules.Classes[klass] = require('./modules' + module[0])[module[1] || klass];
 	}
 
 	return Deck;
